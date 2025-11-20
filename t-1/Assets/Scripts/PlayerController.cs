d8d3e6d0cce09c5c41e27b84e7603a8a9cc0079b
Alexander Marynchev <alemar27@bergen.org>
2025-11-20 12:19:44 -0500

    adds simple player controller mechanics

---
+++ t-1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Scripting.APIUpdating;
+
+public class PlayerController : MonoBehaviour
+{
+    private InputAction moveAction, dashAction, sprintAction, attackAction;
+    private float xAxis, yAxis;
+    [SerializeField] private float moveSpeed = 5f;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        dashAction = InputSystem.actions.FindAction("Dash");
+        sprintAction = InputSystem.actions.FindAction("Sprint");
+        attackAction = InputSystem.actions.FindAction("Attack");
+
+        moveAction.Enable();
+        dashAction.Enable();
+        sprintAction.Enable();
+        attackAction.Enable();
+    }
+
+    void Update()
+    {
+        GetInput();
+        UpdateState();
+        Move();
+    }
+
+    void GetInput()
+    {
+        xAxis = moveAction.ReadValue<Vector2>().x;
+        yAxis = moveAction.ReadValue<Vector2>().y;
+    }
+
+    void UpdateState()
+    {
+        
+    }
+
+    void Move()
+    {
+        transform.position += new Vector3(xAxis, yAxis, 0) * moveSpeed * Time.deltaTime;
+    }
+}
