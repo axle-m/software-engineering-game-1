606482957a53f2e82dad2c0be0b019f0e86002f7
Alexander Marynchev <alemar27@bergen.org>
2025-11-20 15:59:43 -0500

    updates input actions, adds attack hitbox, attack functionailty to be implemented

--- t-1/Assets/Scripts/PlayerController.cs
+++ t-1/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,16 @@ public class PlayerController : MonoBehaviour
     private float xAxis, yAxis;
     [SerializeField] private float moveSpeed = 5f;
 
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRange = 1.5f;
+    private LayerMask enemyLayers;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
@@ -19,6 +29,11 @@ public class PlayerController : MonoBehaviour
         dashAction.Enable();
         sprintAction.Enable();
         attackAction.Enable();
+
+        enemyLayers = LayerMask.GetMask("Enemy");
+        
+        //find and initialize attack point
+        attackPoint = gameObject.transform.GetChild(0);
     }
 
     void Update()
@@ -36,7 +51,20 @@ public class PlayerController : MonoBehaviour
 
     void UpdateState()
     {
-        
+        //flip with movement direction
+        transform.localScale = new Vector3(xAxis == 0 ? transform.localScale.x : Mathf.Sign(xAxis) * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+
+        //update attack point
+        float x = xAxis == 0 ? 0 : attackRange / 2;
+        float y = yAxis == 0 ? 0 : Mathf.Sign(yAxis) * attackRange / 2;
+
+        if(xAxis != 0 && yAxis != 0)
+        {
+            x = attackRange / 2 * 0.7f;
+            y = Mathf.Sign(yAxis) * attackRange / 2 * 0.7f;
+        }
+
+        attackPoint.localPosition = new Vector3(x, y, 0);
     }
 
     void Move()
