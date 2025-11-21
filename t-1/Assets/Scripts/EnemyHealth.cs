f1b8c911b97d60b1239c05dd8e81340c0ab9241f
gabmoj27 <gmojicaschools@gmail.com>
2025-11-21 10:36:17 -0500

    Beginnning enemy health and player attack in place

---
+++ t-1/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
