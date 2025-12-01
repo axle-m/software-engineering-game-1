18d0ade5f86b5271086cc8a67e45ab43d9e8d2f0
MaxN1322 <maxner27@bergen.org>
2025-12-01 09:51:20 -0500

    feature/finished-title-screen

--- t-1/Assets/Scripts/EnemySpawner.cs
+++ t-1/Assets/Scripts/EnemySpawner.cs
@@ -19,6 +19,8 @@ public class EnemySpawner : MonoBehaviour
         {
             SpawnEnemy();
             yield return new WaitForSeconds(spawnInterval);
+            if(spawnInterval > 0.5f)
+            spawnInterval -= 0.05f;
         }
     }
 
