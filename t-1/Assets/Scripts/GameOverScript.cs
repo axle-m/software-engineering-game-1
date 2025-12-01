364a735684390eb218633cac261ba638b7920236
kai-ai-ver32 <kailindemer@gmail.com>
2025-12-01 01:05:37 -0500

    fixed health bar so that it resets to max health after restarting

--- t-1/Assets/Scripts/GameOverScript.cs
+++ t-1/Assets/Scripts/GameOverScript.cs
@@ -4,13 +4,17 @@ using UnityEngine.SceneManagement;
 
 public class GameOverScript : MonoBehaviour
 {
+    public GameManager gameManager;
     public void Setup() {
         gameObject.SetActive(true);
     }
     public void RestartButton(){
         SceneManager.LoadScene("SampleScene");
+        gameManager.HealPlayer(100);
+
     }
     public void ExitButton(){
         SceneManager.LoadScene("StartMenu");
+        gameManager.HealPlayer(100);
     }
 }
